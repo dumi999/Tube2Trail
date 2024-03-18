694eab7cb235f082555fd477fcf44f4614074cb9
Jack Richmond <jacksrichmond@yahoo.co.uk>
2024-03-18 16:20:28 +0000

    activities

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_18_143220) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_18_160937) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "activities", force: :cascade do |t|
+    t.string "name"
+    t.integer "difficulty"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false

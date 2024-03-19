d3f14b391233bc2c318b8c4959ee1d2db62671bd
Jack Richmond <jacksrichmond@yahoo.co.uk>
2024-03-19 16:47:00 +0000

    topnav stage 1

--- app/javascript/navbar.js
+++ app/javascript/navbar.js
@@ -1,8 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("turbo:load", function() {
+  console.log("Document loaded with Turbo");
+
   const burgerMenu = document.getElementById("burger-menu");
   const navbarMenu = document.getElementById("navbar-menu");
 
   burgerMenu.addEventListener("click", function() {
+    console.log("Burger menu clicked");
     navbarMenu.classList.toggle("active");
   });
 });

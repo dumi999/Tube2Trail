b4669b41be5e8d78112932e9ffadfe748b7b8c29
Maria Dumitrita Tomsa <maria.tomsa@protonmail.com>
2024-03-20 16:02:58 +0000

    added a view less option and solved the previous issues with view more

--- Gemfile
+++ Gemfile
@@ -45,7 +45,7 @@ gem "bootsnap", require: false
 
 gem 'kaminari'
 
-gemspec
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,17 +9,3 @@ eagerLoadControllersFrom("controllers", application)
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
-
-  // JavaScript to toggle visibility of full description
-  document.addEventListener("DOMContentLoaded", function() {
-    const viewMoreLinks = document.querySelectorAll('.view-more-link');
-    viewMoreLinks.forEach(function(link) {
-      link.addEventListener('click', function(event) {
-        event.preventDefault();
-        const descriptionContainer = this.closest('.activity-description');
-        const fullDescription = descriptionContainer.querySelector('.full-description');
-        fullDescription.style.display = 'block';
-        this.parentNode.style.display = 'none'; // Hide the "View more" link
-      });
-    });
-  });

2820c9ee534a1769ca28938c10ec9b805d29e570
Sam Ridsdale <samridsdale108@gmail.com>
2024-03-20 14:17:55 +0000

    chatroom back end and some styling

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,2 +1,18 @@
 class ChatroomsController < ApplicationController
+  before_action :set_chatroom, only: [:show]
+
+  def index
+    @chatrooms = Chatroom.all
+  end
+
+  def show
+    @message = Message.new
+    @activity = Activity.find(params[:id])
+  end
+
+  private
+
+  def set_chatroom
+    @chatroom = Chatroom.find(params[:id])
+  end
 end

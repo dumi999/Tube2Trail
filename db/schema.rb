04f15094fc02ddd0a87b3682241684f0bc196b0c
Sam Ridsdale <samridsdale108@gmail.com>
2024-03-21 16:55:17 +0000

    schema file

--- db/schema.rb
+++ db/schema.rb
@@ -107,8 +107,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_20_154537) do
     t.string "username"
     t.text "bio"
     t.string "address"
-    t.text "description"
     t.string "photo"
+    t.text "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

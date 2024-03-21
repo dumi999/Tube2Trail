7d6a1f39b4df5d8f78ebc1f6d0f0b98af5c84a5c
Maria Dumitrita Tomsa <maria.tomsa@protonmail.com>
2024-03-21 15:46:34 +0000

    added styling to index and show page and added a user can like a buton

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create]
   resources :events, only: [:new, :create, :edit, :show, :index, :update]
-  resources :activities, only: [:index, :show]
+  resources :activities, only: [:index, :show] do
+    resources :liked_activities, only: [:create]
+  end
+  resources :liked_activities, only: [:destroy]
   get 'home', to: 'home#index', as: 'home'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.

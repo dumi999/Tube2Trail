38b65f1e07fef2c42e06350ff9bf5fac4b1d274b
Maria Dumitrita Tomsa <maria.tomsa@protonmail.com>
2024-03-28 18:32:23 +0000

    checklist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+  def default_url_options
+    { heroku: ENV["DOMAIN"] || "http://www.tube2trail.site" }
+  end
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :photo])
